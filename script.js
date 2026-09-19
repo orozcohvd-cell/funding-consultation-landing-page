87e1676120b6fe1fa4e5652c764633061610e218
orozcohvd-cell <294532211+orozcohvd-cell@users.noreply.github.com>
2026-09-19 10:00:51 +0800

    chore: update LINE destination

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const message = document.querySelector('.form-message');
 const modal = document.querySelector('#success-modal');
 const consultationCode = document.querySelector('#consultation-code');
 const lineCta = document.querySelector('#line-cta');
-const lineUrl = 'https://lin.ee/9a4T7qeq';
+const lineUrl = 'https://lin.ee/OKRaTKK';
 let selectedFunding = '';
 let currentLeadId = '';
 
